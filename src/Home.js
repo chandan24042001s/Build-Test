f20aad4fcfcd72cf864c66450df1d76446affc0f
Chandan Kumal <85206406+chandan24042001s@users.noreply.github.com>
2024-08-06 17:30:01 +0530

    build setup

---
+++ src/Home.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Home = () => {
+    const data=[
+      {image:'../download.jpeg'},
+      {image:'../images.jpeg'},
+      {image:'../images1.jpeg'},
+      {image:'../cow.jpeg'},
+      {image:'../cow2.jpeg'}
+    ]
+    
+      return (
+        <div className="m-6 p-3 rounded-xl flex flex-row">
+        {data.map((item)=>(
+          <div>
+          <img
+            className="h-[160px] w-10 rounded-2xl"
+            src={item.image}
+            alt="dwonload_image"
+          />
+          <h2 className="font-bold text-lg"> Animal Name: Cow</h2>
+          <h4 className="text-sm">Species: Rare </h4>
+          <div className="bg-LightGreen h-8 rounded-lg w-14 mt-2 text-Secondry flex justify-center items-center text-sm">
+            <i class="fa-solid fa-star p-1"></i>
+            Found: Tamil Naidu
+          </div>
+  
+          
+        </div>
+        ))}
+        
+          </div>
+      );
+}
+
+export default Home
